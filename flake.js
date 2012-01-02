1afa6bc1051e6bd45ebca4d6197e2f79964f4cd4
Gavin Anderegg <ganderegg@mtlpr.ca>
2012-01-01 20:39:44 -0400

    Real pixel doubling instead of CSS stretching

--- flake.js
+++ flake.js
@@ -2,8 +2,8 @@
 
 
 var config = {
-	'width': 320,
-	'height': 240,
+	'width': 320, // The virtual size. These vaules
+	'height': 240, // are doubled when drawn
 	'fps': 30
 };
 
@@ -14,6 +14,10 @@ var screen = {
 };
 
 
+var flakes = [];
+var animate = true;
+
+
 $(document).ready(function() {
 	// from: http://paulirish.com/2011/requestanimationframe-for-smart-animating/
 	window.requestAnimFrame = (function() {
@@ -22,10 +26,19 @@ $(document).ready(function() {
 			window.mozRequestAnimationFrame ||
 			window.oRequestAnimationFrame ||
 			window.msRequestAnimationFrame ||
-		function(/* function */ callback, /* DOMElement */ element) {
-			window.setTimeout(callback, 1000 / 60);
-		};
+			function(/* function */ callback, /* DOMElement */ element) {
+				window.setTimeout(callback, 1000 / 60);
+			};
 	})();
+	
+	$(document).keydown(function(event) {
+		console.log("which: " + event.which);
+		console.log("keyPress: " + event.keyPress);
+		
+		if ((event.which == 27) || (event.keyPress == 27)) {
+			animate = false;
+		}
+	});
 	
 	var c = document.getElementById('screen');
 	screen.canvas = c.getContext('2d');
@@ -35,33 +48,62 @@ $(document).ready(function() {
 });
 
 
-function loop(){
-	requestAnimFrame(loop);
-	render();
-}
-
-
 function render() {
-	
-	
-	setPixel(screen.image, randBetween(0, config.width), randBetween(0, config.height), 256, 256, 256, 256);
+	setPixel(screen.image, randBetween(0, Math.floor(config.width / 2)), randBetween(0, Math.floor(config.height / 2)), 256, 256, 256, 256);
 	
 	screen.canvas.putImageData(screen.image, 0, 0); // at coords 0,0
 }
 
 
+function loop() {
+	if (animate == true) {
+		requestAnimFrame(loop);
+		render();
+	}
+}
+
+
 function setPixel(imageData, x, y, r, g, b, a) {
-	index = (x + y * imageData.width) * 4;
+	index = ((x * 2) + (y * 2) * imageData.width) * 4;
+	
+	// First pixel
 	imageData.data[index+0] = r;
 	imageData.data[index+1] = g;
 	imageData.data[index+2] = b;
 	imageData.data[index+3] = a;
+	
+	// The one beside it
+	imageData.data[index+4] = r;
+	imageData.data[index+5] = g;
+	imageData.data[index+6] = b;
+	imageData.data[index+7] = a;
+	
+	index = ((x * 2) + ((y * 2) + 1) * imageData.width) * 4;
+	
+	// The pixel below the first
+	imageData.data[index+0] = r;
+	imageData.data[index+1] = g;
+	imageData.data[index+2] = b;
+	imageData.data[index+3] = a;
+	
+	// The pixel beside it
+	imageData.data[index+4] = r;
+	imageData.data[index+5] = g;
+	imageData.data[index+6] = b;
+	imageData.data[index+7] = a;
 }
 
 
 function randBetween(from, to) {
 	return Math.floor(Math.random() * (to - from + 1) + from);
 }
+
+
+function flake(x, y, weight) {
+	
+}
+
+
 
 
 
